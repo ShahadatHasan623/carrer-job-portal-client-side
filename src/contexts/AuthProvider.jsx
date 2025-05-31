0541484878b1fa00d0c1f42f310960d50064de2a
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-06-01 02:18:11 +0600

    added jwt token withcreamtial auth provider

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,56 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
-import { auth } from '../firebase/firebase.config';
+import React, { useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
+import axios from "axios";
 
 const provider = new GoogleAuthProvider();
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState();
 
-    const [loading,setLoading]=useState(true)
-    const [user,setUser]=useState()
+  const CreateRegister = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const CreateRegister =(email,password)=>{
-        setLoading(true)
-        return createUserWithEmailAndPassword(auth,email,password)
-    }
+  const CreateSignIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const CreateSignIn =(email,password)=>{
-        setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
-    }
+  const signOutUser = () => {
+    return signOut(auth);
+  };
 
-    const signOutUser =()=>{
-        return signOut(auth)
-    }
+  const googleSignIn = () => {
+    return signInWithPopup(auth, provider);
+  };
 
-    const googleSignIn =()=>{
-        return signInWithPopup(auth, provider)
-    }
-
-
-    useEffect(()=>{
-        const unSubscribe =onAuthStateChanged(auth,CurrentUser =>{
-            setLoading(false)
-            setUser(CurrentUser)
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (CurrentUser) => {
+      setUser(CurrentUser);
+      setLoading(false);
+      if (CurrentUser?.email) {
+        const userData = { email: CurrentUser.email };
+        axios.post("http://localhost:3000/jwt", userData,{
+          withCredentials:true
         })
-        return()=>{
-            unSubscribe()
-        }
-    })
+          .then((res) => {
+            console.log("token after jwt", res.data);
+          })
+          .catch(error =>{
+            console.log(error)
+          });
+      }
+    });
+    return () => {
+      unSubscribe();
+    };
+  });
 
-    const userInfo ={
-        CreateRegister,
-        CreateSignIn,
-        loading,
-        user,
-        signOutUser,
-        googleSignIn
-    }
-    return (
-       <AuthContext value={userInfo}>
-        {children}
-       </AuthContext>
-    );
+  const userInfo = {
+    CreateRegister,
+    CreateSignIn,
+    loading,
+    user,
+    signOutUser,
+    googleSignIn,
+  };
+  return <AuthContext value={userInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
